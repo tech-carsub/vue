21432c2ec0cfb7ae6bb3cf455a09fdaf93dcc37a
Dave Honneffer <pearofducks@gmail.com>
2021-09-29 16:37:44 +0200

    Add terser

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,11 @@ import vue from 'rollup-plugin-vue'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import { getBabelOutputPlugin } from '@rollup/plugin-babel'
+import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
 const isEikBuild = !!process.env.eik
-const outputFile = isEikBuild ? './dist/eik/index.js' : './dist/fabric-vue'
+const outputFile = isEikBuild ? './dist/eik/index.js' : './dist/fabric-vue.js'
 const browsers = 'supports es6-module and > 2% in NO and not dead'
 const external = [
   'vue',
@@ -17,10 +18,11 @@ const plugins = [
   getBabelOutputPlugin({ presets: [['@babel/preset-env', { targets: browsers }]] }),
   nodeResolve(),
   commonjs(),
+  terser()
 ]
 
 export default {
   input: 'index.js',
-  output: { file: outputFile, format: 'es', exports: 'named' },
+  output: { file: outputFile, format: 'es', exports: 'named', sourcemap: true },
   plugins, external,
 }
