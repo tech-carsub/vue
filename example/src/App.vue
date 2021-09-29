c70fb50dd4328acf14137224fd8c9ca6a60d0198
Benjamin Akar <benjaminakar2001@gmail.com>
2021-09-29 15:56:51 +0200

    Remove unused code

--- example/src/App.vue
+++ example/src/App.vue
@@ -2,50 +2,6 @@
   <router-view />
 </template>
 
-<script setup>
-import { h } from 'vue';
-import { RouterLink } from 'vue-router';
-
-const SidebarLink = (props) =>
-  h(
-    RouterLink,
-    {
-      to: props.to,
-      class: 'h5 block rounded-4 pl-8 py-8 font-normal text-gray-700',
-    },
-    () => props.label,
-  );
-</script>
-
-<style scoped>
-.layout {
-  padding-left: 240px;
-}
-@media (max-width: 767px) {
-  .layout {
-    padding-left: 0;
-  }
-}
-.router-link-active {
-  background-color: var(--f-gray-200);
-}
-.vue-logo {
-  height: 24px;
-}
-@media (min-width: 768px) {
-  main {
-    max-width: 1080px;
-  }
-  .sidebar {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    width: 240px;
-  }
-}
-</style>
-
 <style>
 .token {
   font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas,
