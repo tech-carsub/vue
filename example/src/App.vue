41e935b2bd1a2cb3a12e911c402c4e55b0f12611
Benjamin Akar <benjaminakar2001@gmail.com>
2021-10-01 17:47:20 +0200

    Updates

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <router-view />
+  <div v-if="isRoot" class="grid lg:grid-cols-3 gap-16">
+    <div class="lg:col-span-2">
+      <router-view />
+    </div>
+    <div>
+      <f-docs-highlight-box></f-docs-highlight-box>
+    </div>
+  </div>
+  <div v-else>
+    <router-view />
+  </div>
 </template>
 
 <style>
@@ -13,3 +23,13 @@ code {
   color: var(--f-aqua-800);
 }
 </style>
+
+<script>
+export default {
+  computed: {
+    isRoot() {
+      return document.location.pathname == '/';
+    },
+  },
+};
+</script>
