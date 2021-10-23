1ed51e9718a0ff5e091e8974233790c02e0ea5c1
Dave Honneffer <pearofducks@gmail.com>
2021-10-23 14:30:33 +0200

    Drop part of random generation

--- components/util/index.js
+++ components/util/index.js
@@ -1,6 +1,5 @@
 const prefix = Date.now().toString(36).slice(5, 7) + Math.random().toString(36).slice(2, 5)
-let _id = 0
 
-export const id = { default: () => 'f-' + prefix + _id++ }
+export const id = { default: () => 'f-' + prefix }
 export const absentProp = Symbol()
 export { default as fClickable } from './f-clickable'
