837aac06b65009c456208581bbd7f75fecd6952e
Dave Honneffer <pearofducks@gmail.com>
2021-10-26 09:51:07 +0200

    Fix ID generation

--- components/util/index.js
+++ components/util/index.js
@@ -1,5 +1,5 @@
-const prefix = Date.now().toString(36).slice(5, 7) + Math.random().toString(36).slice(2, 5)
+const getSuffix = () => Date.now().toString(36).slice(5, 7) + Math.random().toString(36).slice(2, 5)
 
-export const id = { default: () => 'f-' + prefix }
+export const id = { default: () => 'f-' + getSuffix() }
 export const absentProp = Symbol()
 export { default as fClickable } from './f-clickable'
