37bffc0d3fe38b61577d870931b639567f1ad2ca
Dave Honneffer <pearofducks@gmail.com>
2021-12-22 13:04:09 +0100

    Improve example

--- example/src/examples/ButtonGroup.vue
+++ example/src/examples/ButtonGroup.vue
@@ -4,7 +4,7 @@
 
     <h5 class="mb-16">Button group with buttons</h5>
 
-    <f-button-group :outlined="toggleOutline" :raised="toggleRaised">
+    <f-button-group :outlined="toggleOutline" :raised="toggleRaised" :vertical="toggleVertical">
       <f-button-group-item>
         <f-clickable label @click="alert">Foo</f-clickable>
       </f-button-group-item>
@@ -19,23 +19,13 @@
     <div class="mt-32 mb-48 flex gap-8">
       <f-button utility @click="toggleOutline = !toggleOutline">{{ toggleOutline ? 'Remove' : 'Add' }} outline</f-button>
       <f-button utility @click="toggleRaised = !toggleRaised">{{ toggleRaised ? 'Remove' : 'Add' }} shadow</f-button>
-      <f-button utility @click="toggleSelected = !toggleSelected">{{ toggleSelected ? 'Remove' : 'Add' }} selected UI</f-button>
+      <f-button utility @click="toggleSelected = !toggleSelected">{{ toggleSelected ? 'Remove' : 'Add' }} selected-UI</f-button>
+      <f-button utility @click="toggleVertical = !toggleVertical">Make {{ toggleVertical ? 'horizontal' : 'vertical' }}</f-button>
     </div>
 
     <h5 class="mv-16">Button group with radios</h5>
 
-    <f-button-group outlined>
-      <f-button-group-item :selected="radioModel === 'foo'">
-        <f-clickable label radio v-model="radioModel" value="foo">Foo</f-clickable>
-      </f-button-group-item>
-      <f-button-group-item :selected="radioModel === 'bar'">
-        <f-clickable label radio v-model="radioModel" value="bar">Bar</f-clickable>
-      </f-button-group-item>
-    </f-button-group>
-
-    <div class="mv-16 pv-16 h-16" />
-
-    <f-button-group outlined vertical>
+    <f-button-group outlined :vertical="toggleVertical">
       <f-button-group-item :selected="radioModel === 'foo'">
         <f-clickable label radio v-model="radioModel" value="foo">Foo</f-clickable>
       </f-button-group-item>
@@ -52,6 +42,7 @@ import { fButton, fButtonGroup, fButtonGroupItem, fClickable } from '#components
 const toggleOutline = ref(false)
 const toggleRaised = ref(false)
 const toggleSelected = ref(false)
-const alert = () => window.alert('foo')
+const toggleVertical = ref(false)
+const alert = () => window.alert('Clicked!')
 const radioModel = ref('')
 </script>
