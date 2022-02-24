90a1a9920f2a92e50b0c2c9942ded83a3d3cb42f
Dave Honneffer <pearofducks@gmail.com>
2022-02-24 10:31:16 +0100

    Fix vite vs. new version

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,19 +1,17 @@
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 import drnm from 'drnm'
 import path from 'path'
 import fs from 'fs'
 
-const __dirname = drnm(import.meta.url)
-const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json')))
+const __workdir = drnm(import.meta.url)
+const pkg = JSON.parse(fs.readFileSync(path.join(__workdir, '../package.json')))
 const alias = Object.entries(pkg.imports).reduce((acc, [k, v]) => {
-  acc[k] = path.resolve(path.join(__dirname, '..', v))
+  acc[k] = path.resolve(path.join(__workdir, '..', v))
   return acc
 }, {})
 
-/**
- * @type {import('vite').UserConfig}
- */
-export default {
+export default defineConfig({
   plugins: [vue({
     template: {
       compilerOptions: {
@@ -24,4 +22,4 @@ export default {
   })],
   server: { host: "0.0.0.0", port: 3003 },
   resolve: { alias }
-};
+})
