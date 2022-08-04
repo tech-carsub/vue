65eab238975e14861a0d71bb675dfef75ad89b82
Dave Honneffer <pearofducks@gmail.com>
2022-08-04 13:55:16 +0200

    Improve test

--- test/fBreadcrumbs.test.js
+++ test/fBreadcrumbs.test.js
@@ -5,8 +5,7 @@ import { fBreadcrumbs } from '#components'
 describe('breadcrumbs', () => {
   assert.ok(fBreadcrumbs.name)
 
-  it('v-for slot children', () => {
-    // this is producing a fragment, which is what a v-for would do
+  it('normal', () => {
     const defaultSlot = [
       `<a href="#/foo">Foo</a>`,
       `<a href="#/bar">Bar</a>`,
@@ -17,5 +16,23 @@ describe('breadcrumbs', () => {
     assert.include(wrapper.text(), 'Foo/Bar/Baz')
     assert.include(wrapper.html(), 'class="select-none"')
     assert.ok(html.attributes()['aria-label'])
+  })
+  it('v-for', () => {
+    const BreadcrumbFixture = {
+      template: `
+        <f-breadcrumbs>
+          <a v-for="c in crumbs" :href="c.url">{{ c.title }}</a>
+        </f-breadcrumbs>
+      `,
+      components: { fBreadcrumbs },
+      setup: () => ({
+        crumbs: [
+          { title: 'Foo', url: '/hello' },
+          { title: 'Bar', url: '/fabric' }
+        ]
+      })
+    }
+    const wrapper = mount(BreadcrumbFixture)
+    assert.include(wrapper.text(), 'Foo/Bar')
   })
 })
