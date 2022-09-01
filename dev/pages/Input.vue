9137270f8e001d8145b2b76908b2ecd9ab801613
Dave Honneffer <pearofducks@gmail.com>
2022-09-01 07:45:32 +0200

    Cleanup

--- dev/pages/Input.vue
+++ dev/pages/Input.vue
@@ -18,20 +18,20 @@ const moneyMask = { numeral: true, numeralPositiveOnly: true, numeralIntegerScal
   <div>
     <component-title title="Input" />
 
-    <token>
-      <f-input class="mb-16" #suffix="{ inputElement }" label="A required input with a clear button" hint="A hint" required v-model="inputModel">
+    <token :state="inputModel">
+      <f-input v-model="inputModel" class="mb-16" #suffix="{ inputElement }" label="A required input with a clear button" hint="A hint" required>
         <f-affix suffix clear @click="handleClear(inputElement)" />
       </f-input>
     </token>
 
-    <token>
+    <token :state="inputModel">
       <f-input #prefix v-model="inputModel" label="I have a prefix" class="mb-16" input-wrapper-class="w-1/2">
         <f-affix prefix label="+47" />
       </f-input>
     </token>
 
-    <token>
-      <f-input v-model.number="numericInputModel" optional number type="text" inputmode="numeric" :mask="moneyMask" label="A masked (money) input" :hint="`Model value: <code>${numericInputModel}</code>`" />
+    <token :state="numericInputModel">
+      <f-input v-model.number="numericInputModel" optional number type="text" inputmode="numeric" :mask="moneyMask" label="A masked (money) input" />
     </token>
   </div>
 </template>
